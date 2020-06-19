5767621a8e2b02dede61b570c587e56d98311144
originalusername <54821554+ADoofus@users.noreply.github.com>
2020-06-18 19:27:41 -0700

    Pls work now

--- Android.mk
+++ Android.mk
@@ -36,25 +36,18 @@ LOCAL_SRC_FILES         := ./libbeatsaber-hook_2019_2_1f1_0_1_2.so
 LOCAL_EXPORT_C_INCLUDES := ./extern/beatsaber-hook/shared/
 include $(PREBUILT_SHARED_LIBRARY)
 
-# # Build the CustomUI shared library
-# include $(CLEAR_VARS)
-# LOCAL_MODULE	        := CustomUI_2019_2_1f1_0_1_3
-# LOCAL_SRC_FILES         := ./extern/BeatSaberQuestCustomUI/libs/arm64-v8a/libcustomui_2019_2_1f1_0_1_3.so
-# LOCAL_EXPORT_C_INCLUDES := ./extern/BeatSaberQuestCustomUI/shared
-# include $(PREBUILT_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 # Include the two libraries
 LOCAL_SHARED_LIBRARIES := modloader
 LOCAL_SHARED_LIBRARIES += beatsaber-hook
 # LOCAL_SHARED_LIBRARIES += CustomUI_2019_2_1f1_0_1_3
 LOCAL_LDLIBS     := -llog
-LOCAL_CFLAGS     := -D'MOD_ID="originalsTestModV1"' -D'VERSION="0.1.0"' -I'C:/Program Files/Unity/Editor/Data/il2cpp/libil2cpp'
-LOCAL_MODULE     := originalstestmodv1
+LOCAL_CFLAGS     := -I'c:/Program Files/Unity/Editor/Data/il2cpp/libil2cpp'
+LOCAL_MODULE     := scorepercent
 LOCAL_CPPFLAGS   := -std=c++2a
 LOCAL_C_INCLUDES := ./include ./src
 LOCAL_SRC_FILES  += $(call rwildcard,src/,*.cpp) ./extern/beatsaber-hook/src/inline-hook/And64InlineHook.cpp
 include $(BUILD_SHARED_LIBRARY)
 
 # In order to make this mod work with BMBF, you must provide a zip file with the specific libbeatsaber-hook.so (file copied to the libs directory)
-# and the mod itself (installed as a hook mod)
+# and the mod itself (installed as a hook mod)C:/Program Files/Unity/Editor/Data/il2cpp/libil2cpp
